3653ebe7e13677ce490d27f6ea77112abad373b2
Jürgen Kreileder <juergen.kreileder@empolis.com>
2017-04-28 19:14:17 +0200

    Use [name] instead of [id] in chunk filenames

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -83,7 +83,7 @@ module.exports = function (options) {
        *
        * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
        */
-      chunkFilename: '[id].chunk.js',
+      chunkFilename: '[name].chunk.js',
 
       library: 'ac_[name]',
       libraryTarget: 'var',
